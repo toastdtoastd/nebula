01487e589c24fea9d87f220ed796f2a5b5738370
toastd <toastdtoastd@gmail.com>
2026-03-22 22:14:39 -0700

    Update nebula.lua

--- nebula.lua
+++ nebula.lua
@@ -463,7 +463,7 @@
                 -- Properties
                 name = properties.name or properties.Name or "nebula";
                 size = properties.size or properties.Size or dim2(0, 650, 0, 400);
-                logo = properties.logo or properties.Logo or "rbxassetid://106463595942385";
+                logo = properties.logo or properties.Logo or "rbxassetid://78903805619242";
 
                 selected_tab;
                 items = {};
